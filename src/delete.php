3ee4f0eb8b0ed6440f3c405b86bf97204dea504e
chatora6 <k23rs095@st.kyusan-u.ac.jp>
2025-12-19 03:23:47 +0900

    削除の際のcsv再書き込みにジャンルと日時を追加

--- src/delete.php
+++ src/delete.php
@@ -1,48 +1,41 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-    <title>ファイル一覧</title>
-</head>
-<body>
-<div class="upload_text">
+
 <?php
-   
-    $delete_file =$_POST['delete'];//削除するファイル
-    $filepath='../file/'.$delete_file;
+//削除するファイル
+$deleteFile =$_POST['delete'];
+$DeleteFilePath='../file/'.$deleteFile;
 
-    $csvfile = '../csv/file.csv';//現在のcsvファイル
-    $csvfile_tmp = '../csv/file_tmp.csv';//一時保存するcsvファイル
+//現在のcsvファイル
+$csvfile = '../csv/file.csv';
+//一時保存するcsvファイル
+$csvfileTmp = '../csv/file_tmp.csv';
 
-    $file_csv=fopen($csvfile,"r");//読み込み
-    $filetmp_csv=fopen($csvfile_tmp,"w");//書き込み
+//読み込み
+$csvR=fopen($csvfile,"r");
+//書き込み
+$csvTmpW=fopen($csvfileTmp,"w");
 
-    $csv_data=[];
-    
-    if(unlink($filepath)){
-        echo "ファイル削除に成功しました。";
-        while(($line = fgetcsv($file_csv,1024))!==FALSE){
-            $name = $line[0];
-            $summary = $line[1];
-            if($delete_file != $name){//削除ファイル以外の時
-                fputcsv($filetmp_csv, [$name, $summary]);
-            }
+$csvData=[];
+
+if(unlink($DeleteFilePath)){
+    echo "ファイル削除に成功しました。";
+    while(($line = fgetcsv($csvR,1024))!==FALSE){
+        $name = $line[0];
+        $summary = $line[1];
+        $genre = $line[2];
+        $time = $line[3];
+        if($deleteFile != $name){//削除ファイル以外の時
+            fputcsv($csvTmpW, [$name, $summary,$genre,$time]);
         }
-        fclose($file_csv);//ファイルを閉じる
-        fclose($filetmp_csv);
-
-        copy($csvfile_tmp, $csvfile);
-        $filetmp_csv=fopen($csvfile_tmp,"w");
-        fclose($filetmp_csv);
-        
-    }else{
-        echo "削除に失敗しました。";
     }
+    fclose($csvR);//ファイルを閉じる
+    fclose($csvTmpW);
 
-    echo '<br><a href="./show.php">一覧へ</a> ';
-    ?>
-</div>
-    </body>
-</head>
-</html>
+    copy($csvfileTmp, $csvfile);
+    $csvTmpW=fopen($csvfileTmp,"w");
+    fclose($csvTmpW);
+    
+}else{
+    echo "削除に失敗しました。";
+}
+
+
